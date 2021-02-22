a54f4a6198627d82a914c8f7517cafb63de022e1
Josiah Hartley <josiah.t.hartley@gmail.com>
2021-02-22 12:23:28 -0600

    Copied ElfHash and Created myHash

--- RaylibStarterCPP/main.cpp
+++ RaylibStarterCPP/main.cpp
@@ -20,6 +20,43 @@
 ********************************************************************************************/
 
 #include "raylib.h"
+#include <iostream>
+
+unsigned int ElfHash(unsigned char* data)
+{
+    unsigned int hash = 0;
+    unsigned int x = 0;
+    
+    for (unsigned char* i = data; *i != '\0'; ++i)
+    {
+        hash = (hash << 4) + *i;
+        if ((x = hash & 0xF0000000L) != 0)
+        {
+            hash ^= (x >> 24);
+            hash &= ~x;
+        }
+    }
+
+    return (hash & 0x7FFFFFFF);
+}
+
+unsigned int myHash(unsigned char* info)
+{
+    //create a value to return
+    unsigned int hash = 0;
+    //create a counter for the for loop
+    int x = 0;
+    //for each character in info that is not '\0'
+    for (unsigned char* i = info; *i != '\0'; ++i)
+    {
+        //set the value to return to the value to return times 0602 plus the value of the info at this iteration
+        hash = (hash * 0602) + info[x];
+        //iterate the counter so this for loop loops to the next value in the info char arr
+        x++;
+    }
+    //once all characters have been applied to the info, return the value initialized at the start
+    return hash;
+}
 
 int main(int argc, char* argv[])
 {
@@ -27,12 +64,13 @@ int main(int argc, char* argv[])
     //--------------------------------------------------------------------------------------
     int screenWidth = 800;
     int screenHeight = 450;
-
+    
     InitWindow(screenWidth, screenHeight, "raylib [core] example - basic window");
 
     SetTargetFPS(60);
     //--------------------------------------------------------------------------------------
-
+    int checkSum = 0;
+    unsigned char* input = new unsigned char[0];
 
     // Main game loop
     while (!WindowShouldClose())    // Detect window close button or ESC key
@@ -40,14 +78,14 @@ int main(int argc, char* argv[])
 
         // Update
         //----------------------------------------------------------------------------------
-        // TODO: Update your variables here
-        //----------------------------------------------------------------------------------
-
+        std::cin >> input;
+        checkSum = myHash(input);
+        //----------------------------------------------------------------------------------        
         // Draw
         //----------------------------------------------------------------------------------
         BeginDrawing();
 
-        ClearBackground(RAYWHITE);
+        ClearBackground(GetColor(checkSum));
 
         EndDrawing();
         //----------------------------------------------------------------------------------
